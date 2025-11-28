8cba55588e0eb3abcfe1e3acc21dfbe6d7f12eca
Owen LeJeune <omlejeune@gmail.com>
2025-11-28 13:55:41 -0500

    fix ios crash

--- shared/src/iosMain/kotlin/com/dnfapps/arrmatey/SonarrConfigurationScreenViewController.kt
+++ shared/src/iosMain/kotlin/com/dnfapps/arrmatey/SonarrConfigurationScreenViewController.kt
@@ -1,11 +1,14 @@
 package com.dnfapps.arrmatey
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.ui.window.ComposeUIViewController
-import com.dnfapps.arrmatey.compose.screens.SonarrConfigurationScreen
+import com.dnfapps.arrmatey.compose.screens.ArrConfigurationScreen
+import com.dnfapps.arrmatey.model.InstanceType
 import com.dnfapps.arrmatey.ui.theme.BasicTheme
 
 fun SonarrConfigurationScreenViewController() = ComposeUIViewController {
     BasicTheme {
-        SonarrConfigurationScreen()
+        Column {  }
+//        ArrConfigurationScreen(InstanceType.Sonarr)
     }
 }
